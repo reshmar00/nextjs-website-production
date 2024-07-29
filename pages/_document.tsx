38a3b074ea7921385b72f035250a9b8116b4333a
reshma raghavan <u1430631@umail.utah.edu>
2024-07-29 15:14:17 -0600

    Updated website title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
+          <title> Reshma Raghavan Portfolio</title>
         </Head>
         <body>
           <Main />
